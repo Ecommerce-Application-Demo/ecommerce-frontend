430e1f75be7e81395f3aa093d114b5a9b653e5a7
kingofroy <kingshukr713@gmail.com>
2024-07-03 21:45:14 +0530

    test 2 metatag

--- src/metaTag/dynamicTags.jsx
+++ src/metaTag/dynamicTags.jsx
@@ -39,7 +39,7 @@ const ProfileTag = () => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no" />
         <title>{user} | Profile</title>
-      
+
       </Helmet>
     </div>
   );
@@ -48,19 +48,24 @@ const ProfileTag = () => {
 const SingleProductTag = (product) => {
   console.log(product);
   return (
-      <div className="application">
-          <Helmet>
-              <meta charSet="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no" />
-              <meta property="og:url" content={`https://desicart.vercel.app`} />
-              <meta property="og:type" content="website" />
-              <meta property="og:title" content={`${product.styleName} at DesiCart.`} />
-              <meta property="og:description" content={'product.productDescription'} />
-              <meta property="og:image" content="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/21496840/2023/3/27/08c78085-82bf-460d-84fd-9ba7fb197ed21679912533908-ADIDAS-Men-Tshirts-4331679912533375-1.jpg" />
-              <meta property="image" content={product?.defaultImage ? product?.defaultImage : 'default-image-url'} />
-              <title>{product.styleName} at DesiCart</title>
-          </Helmet>
-      </div>
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no" />
+        <meta property="og:url" content={`https://desicart.vercel.app`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`${product.styleName} at DesiCart.`} />
+        <meta property="og:description" content={'product.productDescription'} />
+        <meta property="og:image" content="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/21496840/2023/3/27/08c78085-82bf-460d-84fd-9ba7fb197ed21679912533908-ADIDAS-Men-Tshirts-4331679912533375-1.jpg" />
+        <meta property="image" content={product?.defaultImage ? product?.defaultImage : 'default-image-url'} />
+        <meta
+          name="description"
+          content="ecommerce website"
+        />
+        <meta property="title" content={`at DesiCart.`} />
+        <title>{product.styleName} at DesiCart</title>
+      </Helmet>
+    </div>
   );
 };
 
