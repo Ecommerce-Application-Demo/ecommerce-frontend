41e18419fbd4607de34475daedb78104c9a7e07d
kingofroy <kingshukr713@gmail.com>
2024-07-03 21:37:24 +0530

    test metatags for single page

--- src/metaTag/dynamicTags.jsx
+++ src/metaTag/dynamicTags.jsx
@@ -46,7 +46,7 @@ const ProfileTag = () => {
 };
 
 const SingleProductTag = (product) => {
-  console.log(product, 'product');
+  console.log(product);
   return (
       <div className="application">
           <Helmet>
@@ -55,10 +55,9 @@ const SingleProductTag = (product) => {
               <meta property="og:url" content={`https://desicart.vercel.app`} />
               <meta property="og:type" content="website" />
               <meta property="og:title" content={`${product.styleName} at DesiCart.`} />
-              <meta property="og:description" content={product.productDescription} />
-              <meta property="og:image" content={product?.defaultImage ? product?.defaultImage : <DesiCartIcon />} />
+              <meta property="og:description" content={'product.productDescription'} />
+              <meta property="og:image" content="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/21496840/2023/3/27/08c78085-82bf-460d-84fd-9ba7fb197ed21679912533908-ADIDAS-Men-Tshirts-4331679912533375-1.jpg" />
               <meta property="image" content={product?.defaultImage ? product?.defaultImage : 'default-image-url'} />
-              <img src={product?.defaultImage ? product?.defaultImage : 'default-image-url'} />
               <title>{product.styleName} at DesiCart</title>
           </Helmet>
       </div>
